6a511f0f7dc28a896f9f61c0a21ed0bae16be9e8
aesencodes <adjiesuryanugraha1@gmail.com>
2022-10-02 16:05:23 +0700

    Added task_2

---
+++ task_2/account.php
@@ -0,0 +1,11 @@
+<?php
+$name = $_GET['name'];
+$email = $_GET['email'];
+$password = $_GET['password'];
+
+echo "Welcome " .$name;
+echo "<br/>";
+echo "Your Email is : " .$email;
+echo "<br/>";
+echo "Your Password is : " .$password;
+?>
